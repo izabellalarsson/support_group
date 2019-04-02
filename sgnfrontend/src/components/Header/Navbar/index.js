f5cea4008eae3ae71c12192f57437032bf05a214
Izabella <izzwii.izabella@gmail.com>
2019-04-02 10:27:24 +0200

    added components for header

---
+++ sgnfrontend/src/components/Header/Navbar/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from ''
+
+
+
+const Navbar = props => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+Navbar.propTypes = {
+    
+};
+
+export default Navbar;
