914926f148c4125c50c95197f09fdd47226fbbb2
Izabella <izzwii.izabella@gmail.com>
2019-04-05 12:06:06 +0200

    added express and slug routes

---
+++ nextjs/pages/branch.js
@@ -0,0 +1,10 @@
+
+import { Fragment } from 'react'
+import Layout from '../components/Layout';
+import Branches from '../components/Branches';
+
+export default () => (
+    <Layout>
+        <h2>hej</h2>
+    </Layout >
+)
---
+++ nextjs/routes.js
@@ -0,0 +1,6 @@
+const routes = require('next-routes');
+
+module.exports = routes()
+    .add('index', '/')
+    .add('branches')
+    .add('branch', '/branches/:slug');
