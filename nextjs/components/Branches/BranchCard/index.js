864d94a54bc66925568ebe54d4a8958b96576422
Izabella <izzwii.izabella@gmail.com>
2019-04-09 14:00:01 +0200

    branchcard styled

--- nextjs/components/Branches/BranchCard/index.js
+++ nextjs/components/Branches/BranchCard/index.js
@@ -10,9 +10,9 @@ const BranchCardStyle = styled.div`
 display: flex;
 flex-direction: column;
 width: 100%;
-
     a {
         text-decoration: none;
+        z-index: 998;
     }
 
     i {
@@ -24,6 +24,39 @@ width: 100%;
         margin-left: 30px;
         display: flex;
         align-items: center;
+        margin-top: 15px;
+    }
+
+    section {
+        display: flex;
+        flex-direction: column;
+        background: #F3F5FF;
+        margin-top: -20px;
+    }
+
+    .fa-exclamation-circle {
+        font-size: 42px;
+        color: #3B5998;
+        align-self: flex-end;
+        margin: 30px;
+    }
+
+    article {
+        display: flex;
+    }
+    
+    article .one {
+        display: flex;
+        align-items: center;
+        background: #fff;
+        width: 60%;
+    } 
+
+    article .two {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 40%;
     }
 `
 
@@ -31,14 +64,25 @@ const Branch = ({name, shortDescription, link, id, city}) => {
     return (
         <BranchCardStyle>
             <Link id={id} href={`/branches/${link}`} ><a href={`/branches/${link}`}><Title text={name}/></a></Link>
-            <div className="where">
-                <i class="fas fa-map-marker-alt"></i>
-                <Subheading text={city}/> 
-            </div>
+            <section>
+                <div className="where">
+                    <i class="fas fa-map-marker-alt"></i>
+                    <Subheading text={city}/> 
+                </div>
 
-            <Text dangerouslySetInnerHTML={{__html: shortDescription}} text={shortDescription}/>
-            
-            <ReadMore/>
+                <Text dangerouslySetInnerHTML={{__html: shortDescription}} text={shortDescription}/>
+                
+                <ReadMore/>
+                <article>
+                    <atricle className="one">
+                    <Text text="Visit the Facebook-page for more info"/>
+                    </atricle>
+
+                    <article className="two">
+                        <i class="fas fa-exclamation-circle"></i>
+                    </article>
+                </article>
+            </section>
         </BranchCardStyle>
     );
 };
