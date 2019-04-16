a4f4a05b4bfa7821995d69a4d379b28e473cc508
Per Baltzar <42942103+perbaltzar@users.noreply.github.com>
2019-04-16 19:24:51 +0200

    added not found to branches

--- nextjs/pages/singleBranch.js
+++ nextjs/pages/singleBranch.js
@@ -3,28 +3,66 @@ import React, { Component } from 'react'
 import Layout from '../components/Layout';
 import fetch from 'isomorphic-unfetch';
 import Title from '../components/Title';
+import Text from '../components/Text';
 
 class BranchPage extends Component {
 
     constructor(props){
         super()
         const slug = props.url.query.slug;
-        //console.log(props)
         const newBranch = props.branches.filter(branch => {
             return (branch.slug === slug)
         })
+        
         this.state = {
-            branch: newBranch[0]
+            notFound: false,
+            branch: newBranch[0],
+            activities: []
         }
-        console.log(this.state.branch);
 
+        // Check if slug excists
+        if (newBranch.length < 1) {
+            //Redirect to 404;
+            console.log('hej');
+            this.state = {
+                notFound: true
+            }
+        }else{
+            const activitiesApi = this.state.branch._links['wp:term'][0].href
+            //console.log(activitiesApi);
+            fetch(activitiesApi)
+            .then(res => res.json())
+            .then(activities => {
+                this.setState({
+                    activities: activities
+                })
+            })
+        }    
     }
 
     
     render () {
         return (
+            
             <Layout>
-                <Title text={this.state.branch.title.rendered} />
+                {this.state.notFound && 
+                <div>
+                    <Title text={'Branch Not Found'} /> 
+                    <Text text="Please try again" />
+                </div>
+                } 
+
+                {!this.state.notFound && 
+                    <div>
+                        <Title text={this.state.branch.title.rendered} />
+                        {
+                            this.state.activities.map(activity => {
+                                console.log(activity.name)
+                                return <Text text={activity.name} />
+                            })
+                        }
+                    </div>
+                }
             </Layout>
         )
     }
