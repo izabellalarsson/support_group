f4059406bf75ef83e57d614bd20a5f7cd4ff9d26
Izabella <izzwii.izabella@gmail.com>
2019-04-17 13:55:52 +0200

    added logo in navbar

--- nextjs/components/Header/Search/index.js
+++ nextjs/components/Header/Search/index.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 const SearchStyled = styled.div`
-    margin-left: 200px;
-    i {
-        font-size: 20px;
-    }
-    
-`
+  margin-left: 200px;
+  i {
+    font-size: 20px;
+  }
+`;
 
-const Search = (props) => {
-    return (
-        <SearchStyled onClick={props.handleClickSearch} >
-            <i className="fas fa-search"></i>
-        </SearchStyled>
-    );
+const Search = props => {
+  return (
+    <SearchStyled onClick={props.handleClickSearch}>
+      <i className='fas fa-search'> </i>{" "}
+    </SearchStyled>
+  );
 };
 
 export default Search;
