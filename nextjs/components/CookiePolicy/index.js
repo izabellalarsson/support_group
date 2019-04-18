b7d5ed7dcd4fe55810dc55f1ea84034b963cd115
Per Baltzar <42942103+perbaltzar@users.noreply.github.com>
2019-04-18 15:17:23 +0200

    cookies policy design

--- nextjs/components/CookiePolicy/index.js
+++ nextjs/components/CookiePolicy/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import styled from 'styled-components'
-import Button from '../Button';
+import Text from '../Text';
 
 const CookiePolicyStyled = styled.div`
     width: 100vw;
-    height: 10vh;
-    position: sticky;
+    position: fixed;
+    z-index: 999;
     bottom: 0vh;
     background: var(--sub-bg);
-    justify-content: space-around;
-    align-items: center;
+    flex-flow: column;
     display: ${props => props.isCookiesApproved ? 'none' : 'flex'};
     button{
+        margin-right: 30px;
+        margin-bottom: 30px;
+        width: 150px;
         height: 28px;
         background-color: var(--detail-red);
         color: white;
@@ -22,13 +24,26 @@ const CookiePolicyStyled = styled.div`
         border: none;
         outline:none;
     }
+    p{
+        padding-bottom: 0;
+    }
+    div{
+        display: flex;
+        justify-content: flex-end;
+    }
 `
 
 const CookiePolicy = ({isCookiesApproved, handleCookieButtonClick}) => {
     return (
         <CookiePolicyStyled isCookiesApproved={isCookiesApproved}>
-            Do you approve of cookies?
-            <button onClick={handleCookieButtonClick}> I Understand</button>
+            <Text text="We and our partners use cookies on this site to improve our service, 
+            perform analytics, personalize advertising, measure advertising performance, 
+            and remember website preferences. By using the site, 
+            you consent to these cookies.For more information on cookies 
+            including how to manage your consent visit our cookie policy." />
+            <div>
+                <button onClick={handleCookieButtonClick}>Continue</button>
+            </div>
         </CookiePolicyStyled>
     );
 };
