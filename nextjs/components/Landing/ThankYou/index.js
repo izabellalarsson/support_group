6da26d5e747cd698d1972de692fcd2f4d88a8901
Izabella <izzwii.izabella@gmail.com>
2019-04-18 11:51:48 +0200

    landing page done

--- nextjs/components/Landing/ThankYou/index.js
+++ nextjs/components/Landing/ThankYou/index.js
@@ -2,40 +2,59 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import Title from "../../Title";
-
+import Subheading from "../../Subheading";
 const ThankYouStyled = styled.div`
   /* overflow: hidden; */
   /* scroll-snap-type: x mandatory; */
   div {
-    height: 80vh;
     width: 100vw;
-    margin-top: -20px;
-    padding: 0;
-    background: var(--main-bg);
+    /* background: red; */
     margin-bottom: 55px;
     display: flex;
     scroll-snap-points-y: repeat(100vw);
     scroll-snap-type: x mandatory;
     overflow-x: scroll;
+    height: 50vh;
+    position: relative;
+    padding-top: 5rem;
   }
 
-  section {
-    border-right: 1px solid white;
-    padding: 1rem;
-    min-width: 100vw;
-    height: 80vh;
+  div section {
+    padding-right: 0.5rem;
+    min-width: 90vw;
+    height: 35vh;
     scroll-snap-align: start;
-    position: relative;
+    /* position: relative; */
+    background: rgba(0, 0, 0, 0);
   }
   img {
-    width: 90%;
-    height: 50%;
+    width: 100%;
+    height: 100%;
     object-fit: cover;
     object-position: 50% 50%;
     margin: 15px;
-    position: absolute;
-    top: 50%;
-    transform: translateY(-80%);
+    /* position: absolute; */
+    /* top: 50%; */
+    /* transform: translateY(-80%); */
+  }
+
+  h1 {
+    position: relative;
+  }
+
+  section {
+    height: 60vh;
+    width: 90%;
+    margin-top: -20px;
+    padding: 0;
+    background: var(--main-bg);
+    margin-bottom: 55px;
+    align-self: flex-start;
+  }
+
+  h2 {
+    margin-top: 0;
+    margin-bottom: 0;
   }
 `;
 
@@ -43,14 +62,19 @@ const ThankYou = props => {
   return (
     <ThankYouStyled>
       <Title text='Thank you' />
-      <div>
-        <section>
-          <img src='https://images.unsplash.com/photo-1429962714451-bb934ecdc4ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80' />
-        </section>
-        <section>
-          <img src='https://images.unsplash.com/photo-1444210971048-6130cf0c46cf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1652&q=80' />
-        </section>
-      </div>
+      <section>
+        <div>
+          <section>
+            <img src='https://images.unsplash.com/photo-1429962714451-bb934ecdc4ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80' />
+            <Subheading text='Izabella Larsson' />
+          </section>
+          <section>
+            <img src='https://images.unsplash.com/photo-1444210971048-6130cf0c46cf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1652&q=80' />
+            <Subheading text='Per Baltzar' />
+            <Subheading text='Member since 2019' />
+          </section>
+        </div>
+      </section>
     </ThankYouStyled>
   );
 };
--- nextjs/components/Members/JourneyCard/index.js
+++ nextjs/components/Members/JourneyCard/index.js
@@ -1,57 +1,53 @@
-import React from 'react';
-import styled from 'styled-components';
-import Text from '../../Text';
-import Title from '../../Title';
-import BlueLine from '../../BlueLine';
-import ReadMore from '../../ReadMore';
+import React from "react";
+import styled from "styled-components";
+import Text from "../../Text";
+import Title from "../../Title";
+import BlueLine from "../../BlueLine";
+import ReadMore from "../../ReadMore";
 
 const JourneyCardStyled = styled.section`
-
-background-color: white;
-margin-bottom: 60px;
-margin-top: 130px;
-img {
+  background-color: white;
+  margin-bottom: 60px;
+  img {
     width: 100%;
     height: 40vh;
     object-fit: cover;
     object-position: 50% 50%;
     margin-top: 4vh;
     margin-bottom: 60px;
-}
-div:nth-child(2){
+  }
+  div:nth-child(2) {
     position: absolute;
     background-color: white;
     margin: 31vh auto 0 auto;
     width: 50%;
     height: 13vh;
-    
-}
-div:nth-child(2) p{
+  }
+  div:nth-child(2) p {
     margin-left: 30px;
     font-size: 12px;
-}
-div:nth-child(5) {
+  }
+  div:nth-child(5) {
     display: flex;
     justify-content: flex-end;
-}
-`
-const JourneyCard = ({name, age, memberSince, src}) => {
-    return (
-         <JourneyCardStyled>
-            <Title text="This is my SGN journey" />
-            <div>            
-                <p>{name}</p>
-                <p>{`${age} years old`}</p>
-                <p>{`Member since ${memberSince}`}</p>  
-            </div>
-            <img src={src} alt="person" />
-            <Text text="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />     
-            <div>
-                <ReadMore />
-            </div>
-            <BlueLine />
-        </JourneyCardStyled>
-    );
+  }
+`;
+const JourneyCard = ({ name, age, memberSince, src }) => {
+  return (
+    <JourneyCardStyled>
+      <Title text='This is my SGN journey' />
+      <div>
+        <p> {name} </p> <p> {`${age} years old`} </p>{" "}
+        <p> {`Member since ${memberSince}`} </p>{" "}
+      </div>{" "}
+      <img src={src} alt='person' />
+      <Text text='Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' />
+      <div>
+        <ReadMore />
+      </div>{" "}
+      <BlueLine />
+    </JourneyCardStyled>
+  );
 };
 
 export default JourneyCard;
