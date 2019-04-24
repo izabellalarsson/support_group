88cc8902840a511978312b02ac6e1c35b0b3a0f1
Per Baltzar <42942103+perbaltzar@users.noreply.github.com>
2019-04-24 16:36:46 +0200

    merge

--- nextjs/components/Projects/index.js
+++ nextjs/components/Projects/index.js
@@ -65,7 +65,6 @@ export default class Projects extends Component {
 
 
   render() {
-      console.log(this.state.projects)
     return (
       <div>
         {this.state.projects.map((project, i) => (
