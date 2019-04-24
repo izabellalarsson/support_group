91383e8613da3dadc1fa855f53ab499105aac645
Izabella <izzwii.izabella@gmail.com>
2019-04-24 14:23:37 +0200

    fixed animation read me

--- nextjs/components/ReadMore/index.js
+++ nextjs/components/ReadMore/index.js
@@ -1,86 +1,61 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import Link from 'next/link';
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import Link from "next/link";
 
 const ReadMoreStyle = styled.div`
-align-self: flex-end;
-width: 30%;
-height: 20px;
-margin-bottom: 8px;
+  align-self: flex-end;
+  width: 30%;
+  height: 20px;
+  margin-bottom: 8px;
 
-a {
-    animation-name: move;
-    animation-duration: 3s;
-    animation-iteration-count: infinite;  
+  a {
     text-decoration: none;
-    color: #EE7171;
-}
+    color: #ee7171;
+  }
 
-a:hover {
-    animation-iteration-count: 0; 
-    font-weight: bold; 
+  a:hover {
+    animation-iteration-count: 0;
+    font-weight: bold;
 
-
-    +hr {
-        animation-name: stripe;
-        animation-duration: 0.5s;
-        animation-iteration-count: 1;  
-        border-top: 3px solid #EE7171;
-        display: block;
-        width: 80%;
-        margin: 0;
-        margin-bottom: 10px;
+    + hr {
+      animation-name: stripe;
+      animation-duration: 0.5s;
+      animation-iteration-count: 1;
+      border-top: 3px solid #ee7171;
+      display: block;
+      width: 80%;
+      margin: 0;
+      margin-bottom: 10px;
     }
-}
-
-hr {
-  display: none;
-}
-
-
-@keyframes stripe {
-  0% {
-    width: 0;
   }
 
-  100% {
-    width: 80%;
-  }
-}
-
-@keyframes move {
-  0% {
-    font-weight: 200;
+  hr {
+    display: none;
   }
 
-  50% {
-    font-weight: 200;
+  @keyframes stripe {
+    0% {
+      width: 0;
+    }
+
+    100% {
+      width: 80%;
+    }
   }
+`;
 
-  75% {
-      font-weight: 600;
-  }
-
-  100% {
-    font-weight: 200;
-  }
-
-}
-`
-
-
-const ReadMore = ({link, type}) => {
-    return (
-        <ReadMoreStyle>
-            <Link href={`/${type}/${link}`}><a href={`/${type}s/${link}`}>Read more</a></Link>
-            <hr/>
-        </ReadMoreStyle>
-    );
+const ReadMore = ({ link, type }) => {
+  return (
+    <ReadMoreStyle>
+      <Link href={`/${type}/${link}`}>
+        <a href={`/${type}s/${link}`}> Read more </a>
+      </Link>
+      <hr />
+    </ReadMoreStyle>
+  );
 };
 
-ReadMore.propTypes = {
-    
-};
+ReadMore.propTypes = {};
 
 export default ReadMore;
